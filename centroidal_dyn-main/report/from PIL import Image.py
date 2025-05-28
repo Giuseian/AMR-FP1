16f29c4f0778c573fe22a0729a2d9c49ce345534
Giuseian <giuseppina.iannotti23@gmail.com>
2025-05-28 16:45:21 +0200

    updated still task and report

--- centroidal_dyn-main/report/from PIL import Image.py
+++ centroidal_dyn-main/report/from PIL import Image.py
@@ -1,9 +1,9 @@
 from PIL import Image
 
 # Open the images
-img1 = Image.open("C:/Users/giuse/OneDrive/Desktop/GITHUB PROJECTS/AMR-FP1/centroidal_dyn-main/plots/Feet_z walking.png")
-img2 = Image.open("C:/Users/giuse/OneDrive/Desktop/GITHUB PROJECTS/AMR-FP1/centroidal_dyn-main/plots/CoM Velocity walking.png")
-img3 = Image.open("C:/Users/giuse/OneDrive/Desktop/GITHUB PROJECTS/AMR-FP1/centroidal_dyn-main/plots/Angular Momentum walking.png")
+img1 = Image.open("C:/Users/giuse/OneDrive/Desktop/GITHUB PROJECTS/AMR-FP1/centroidal_dyn-main/plots/CoM x Trajectory walking.png")
+img2 = Image.open("C:/Users/giuse/OneDrive/Desktop/GITHUB PROJECTS/AMR-FP1/centroidal_dyn-main/plots/CoM y Trajectory walking.png")
+img3 = Image.open("C:/Users/giuse/OneDrive/Desktop/GITHUB PROJECTS/AMR-FP1/centroidal_dyn-main/plots/CoM z Trajectory walking.png")
 
 # Resize all to the same width and height
 width, height = 600, 400  # Adjust as needed
@@ -12,6 +12,6 @@ img2 = img2.resize((width, height))
 img3 = img3.resize((width, height))
 
 # Save the adjusted images
-img1.save("C:/Users/giuse/OneDrive/Desktop/GITHUB PROJECTS/AMR-FP1/centroidal_dyn-main/plots/Feet_z walking.png")
-img2.save("C:/Users/giuse/OneDrive/Desktop/GITHUB PROJECTS/AMR-FP1/centroidal_dyn-main/plots/CoM Velocity walking.png")
-img3.save("C:/Users/giuse/OneDrive/Desktop/GITHUB PROJECTS/AMR-FP1/centroidal_dyn-main/plots/Angular Momentum walking.png")
+img1.save("C:/Users/giuse/OneDrive/Desktop/GITHUB PROJECTS/AMR-FP1/centroidal_dyn-main/plots/CoM x Trajectory walking.png")
+img2.save("C:/Users/giuse/OneDrive/Desktop/GITHUB PROJECTS/AMR-FP1/centroidal_dyn-main/plots/CoM y Trajectory walking.png")
+img3.save("C:/Users/giuse/OneDrive/Desktop/GITHUB PROJECTS/AMR-FP1/centroidal_dyn-main/plots/CoM z Trajectory walking.png")
